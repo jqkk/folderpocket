4e12d44fc54632f7828166adb25604478b15ec65
jqkk <kkj2551@naver.com>
2023-04-03 21:29:28 +0900

    :sparkles: feat(#01): arrow icon 배치

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { TriangleDownIcon } from '@chakra-ui/icons';
 import { Box } from '@chakra-ui/react';
 
 import FolderInput from '@/domains/components/FolderInput';
@@ -6,6 +7,9 @@ const Home = () => {
   return (
     <Box px={7} py={10}>
       <FolderInput />
+      <Box display='flex' justifyContent='center' my={2}>
+        <TriangleDownIcon boxSize={6} color='yellow.400' />
+      </Box>
     </Box>
   );
 };
