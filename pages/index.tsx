a9f2858c75176ae4a38ca3b493c539099246de4b
jqkk <kkj2551@naver.com>
2023-04-03 21:55:14 +0900

    :sparkles: feat(#01): 마크업 완료

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,24 @@
 import { TriangleDownIcon } from '@chakra-ui/icons';
-import { Box } from '@chakra-ui/react';
+import { Box, Button } from '@chakra-ui/react';
 
 import FolderInput from '@/domains/components/FolderInput';
+import SearchGithub from '@/domains/components/SearchGithub';
+import TreeOutput from '@/domains/components/TreeOutput';
 
 const Home = () => {
   return (
     <Box px={7} py={10}>
+      <Box mb={7}>
+        <SearchGithub />
+      </Box>
       <FolderInput />
       <Box display='flex' justifyContent='center' my={2}>
         <TriangleDownIcon boxSize={6} color='yellow.400' />
       </Box>
+      <TreeOutput />
+      <Button width='100%' mt={5} colorScheme='yellow'>
+        Copy
+      </Button>
     </Box>
   );
 };
