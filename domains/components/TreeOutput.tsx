90ea3bdee1fafa6c3114276f5dce70ed7774f7f8
jqkk <kkj2551@naver.com>
2023-04-03 21:53:29 +0900

    :sparkles: feat(#01): TreeOutput 컴포넌트 추가

---
+++ domains/components/TreeOutput.tsx
@@ -0,0 +1,13 @@
+import { Box } from '@chakra-ui/react';
+
+const TreeOutput = () => {
+  return (
+    <Box
+      borderColor='gray.100'
+      borderWidth='1px'
+      minHeight={60}
+      width='100%'></Box>
+  );
+};
+
+export default TreeOutput;
